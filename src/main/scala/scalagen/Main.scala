d5998fe7cc45fa7e7d393cee9f7fc4a911331e68
aoiroaoino <aoiro.aoino@gmail.com>
2020-03-08 21:10:46 +0900

    update Main

--- src/main/scala/scalagen/Main.scala
+++ src/main/scala/scalagen/Main.scala
@@ -1,66 +1,136 @@
 package scalagen
 
+import scala.util.chaining._
+
 object Main {
+  import visitors._
 
-  // type AccountID = Int
-  // type Account = {
-  //   id: AccountID,
-  //   name: String
-  // }
-  val sample1: Seq[NamedType] = Seq(
-    NamedType("AccountID", Intrinsic.Int()),
-    NamedType(
-      "Account",
-      Struct(
-        "id"   -> TypeRef("AccountID"),
-        "name" -> Intrinsic.String()
+  val example = new {
+
+    // type AccountId = Int
+    val accountId: Tree = Defn.Type(Type.Name("AccountId"), Type.Int)
+
+    // type Account = {
+    //   id: AccountId,
+    //   name: String
+    // }
+    val account: Tree =
+      Defn.Type(
+        Type.Name("Account"),
+        Type.Struct(
+          Term.Field(Term.Name("id"), Type.Name("AccountId")),
+          Term.Field(Term.Name("name"), Type.String)
+        )
       )
-    )
-  )
 
-  // type User = {
-  //   name: String,
-  //   age: Int,
-  //   address: {
-  //     postcode: String,
-  //     city: String
-  //   },
-  //   isActive: Boolean
-  // }
-  val sample2: Seq[NamedType] = Seq(
-    NamedType(
-      "User",
-      Struct(
-        "name" -> Intrinsic.String(),
-        "age"  -> Intrinsic.Int(),
-        "address" -> Struct(
-          "postcode" -> Intrinsic.String(),
-          "city"     -> Intrinsic.String()
-        ),
-        "isActive" -> Intrinsic.Boolean()
+    // type User = {
+    //   name: String,
+    //   age: Int,
+    //   address: {
+    //     postcode: String,
+    //     city: {
+    //       name: String,
+    //     },
+    //     pos: {
+    //       lan: Float,
+    //       lon: Float
+    //     }
+    //   },
+    //   isActive: Boolean
+    // }
+    val user: Tree =
+      Defn.Type(
+        Type.Name("User"),
+        Type.Struct(
+          Term.Field(Term.Name("name"), Type.String),
+          Term.Field(Term.Name("age"), Type.Int),
+          Term.Field(
+            Term.Name("address"),
+            Type.Struct(
+              Term.Field(Term.Name("postcode"), Type.String),
+              Term.Field(Term.Name("city"),
+                         Type.Struct(
+                           Term.Field(Term.Name("name"), Type.String),
+                         )),
+              Term.Field(Term.Name("pos"),
+                         Type.Struct(
+                           Term.Field(Term.Name("lat"), Type.Float),
+                           Term.Field(Term.Name("lon"), Type.Float)
+                         ))
+            )
+          ),
+          Term.Field(Term.Name("isActive"), Type.Boolean)
+        )
       )
-    )
-  )
 
-  val sample3 = sample1 ++ sample2
+    val trees = List(accountId, account, user)
+  }
 
   def main(args: Array[String]): Unit = {
 
-    {
-      println("====== SyntaxDirectedTranslator")
-      val v = new SyntaxDirectedVisitor()
-      println("=== sample1:")
-      sample1.foreach(v.visit)
-      println("=== sample2:")
-      sample2.foreach(v.visit)
+    // ==============================
+    // === Visitor
+    // ===
+    section("PrintVisitor") {
+      comment("まずは雑に print してみる。綺麗にはならないですね。") {
+        val v = new PrintVisitor
+        example.trees.foreach(v.accept)
+      }
+    }
+    section("PrettyPrintVisitor") {
+      comment("AST を走査しながら各ノードで文字列を組み立てる。AST から DSL を復元できた。") {
+        val v = new PrettyPrintVisitor(indentSpaceSize = 2)
+        example.trees.foreach(v.accept)
+        println(v.result)
+      }
+      comment("頑張れば簡易的な formatter が作れるかも?") {
+        val v = new PrettyPrintVisitor(indentSpaceSize = 8)
+        example.trees.foreach(v.accept)
+        println(v.result)
+      }
+    }
+    section("CollectTermNameVisitor") {
+      comment("AST に含まれる Term.Name を全て列挙する。必要な情報を集める、といった使い方も可能。") {
+        val v = new CollectTermNameVisitor
+        example.trees.foreach(v.accept)
+        println(v.result)
+      }
+    }
+    section("DirectTranslationVisitor") {
+      val v = new DirectTranslationVisitor
+      comment("一見問題なく展開できているように見える") {
+        v.accept(example.account)
+        println(v.result)
+      }
+      comment("しかし、シンプルに実装しただけでは型エイリアスへの展開が考慮できない") {
+        v.accept(example.accountId)
+        println(v.result)
+      }
+      comment("また、DSL は匿名 Struct を許容するが、これをトップレベルの case class としてうまく切り出すことが難しい") {
+        v.accept(example.user)
+        println(v.result)
+      }
+    }
+    section("ModelDrivenTranslationVisitor") {
+      val v = new ModelDrivenTranslationVisitor
     }
 
-    {
-      println("===== ModelDrivenTranslator")
-      val v = new ModelDrivenTranslator()
-      println("=== sample3:")
-      sample3.foreach(v.visit)
-      v.run().foreach(println)
-    }
+    // ==============================
+    // === Traverser and Transformer
+    // ===
+    section("CollectTermNameTraverser") {}
+
+    section("IntToFloatTransformer") {}
+
+    section("TwoPhaseTranslation") {}
   }
+
+  private def section(s: String)(proc: => Unit): Unit =
+    s"""
+       |/**
+       | * $s
+       | */
+       |""".stripMargin.pipe(println).tap(_ => proc)
+
+  private def comment(s: String)(proc: => Unit): Unit = println(s"// $s").tap(_ => proc)
 }
