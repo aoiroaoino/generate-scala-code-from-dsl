d7715b01ac3f17489ec9590f2b43d503e801a530
aoiroaoino <aoiro.aoino@gmail.com>
2020-03-08 21:06:03 +0900

    add util

---
+++ src/main/scala/scalagen/util/ScalaReservedWord.scala
@@ -0,0 +1,50 @@
+package scalagen.util
+
+object ScalaReservedWord {
+
+  def isReservedWord(s: String): Boolean = all.contains(s)
+
+  def escape(s: String): String = if (isReservedWord(s)) s"`$s`" else s
+
+  val all: Set[String] = Set(
+    "abstract",
+    "case",
+    "catch",
+    "class",
+    "def",
+    "do",
+    "else",
+    "extends",
+    "false",
+    "final",
+    "finally",
+    "for",
+    "forSome",
+    "if",
+    "implicit",
+    "import",
+    "lazy",
+    "match",
+    "new",
+    "null",
+    "object",
+    "override",
+    "package",
+    "private",
+    "protected",
+    "return",
+    "sealed",
+    "super",
+    "this",
+    "throw",
+    "trait",
+    "try",
+    "true",
+    "type",
+    "val",
+    "var",
+    "while",
+    "with",
+    "yield"
+  )
+}
